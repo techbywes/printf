6f4341007d7f9daf0b29bde18707f8309f7d6a41
ChahrazadBouragba <91282115+ChahrazadBouragba@users.noreply.github.com>
2023-03-24 17:31:32 +0000

    get_width.c
    get_width.c

--- get_width.c
+++ get_width.c
@@ -2,11 +2,11 @@
 
 /**
  * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
  *
- * Return: width.
+ * Return: width
  */
 int get_width(const char *format, int *i, va_list list)
 {
